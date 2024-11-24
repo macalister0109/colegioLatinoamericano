7f8ae151307150d668432d488ef12f31c0dab00f
Macarena Jamett <82476477+macalister0109@users.noreply.github.com>
2024-11-24 17:08:04 -0300

    crear el menu

---
+++ src/main/java/vistas/Menu.java
@@ -0,0 +1,41 @@
+package vistas;
+
+public class Menu extends MenuTemplate{
+
+	@Override
+	protected void exportarDatos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void crearAlumno() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void agregarMateria() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void agregarNotaPasoUno() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void listarAlumnos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void terminarPrograma() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
